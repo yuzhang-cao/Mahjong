b7c1fdf6f50e28ca2683f16919aa7fccbb98232c
cao yuzhang <caoyuzhang05@gmail.com>
2026-01-03 21:28:38 +0800

    Initial Commit

---
+++ MahjongTing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MahjongTing
+//
+//  Created by caoyuzhang on 1/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MahjongTing/MahjongTingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MahjongTingApp.swift
+//  MahjongTing
+//
+//  Created by caoyuzhang on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MahjongTingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
